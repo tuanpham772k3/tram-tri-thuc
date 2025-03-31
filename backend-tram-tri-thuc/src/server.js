6d0cd6cfc57305c4145198aac9e3e9416141c5b5
TuanAP <tuanphamu23@gmail.com>
2025-03-31 19:26:24 +0700

    connect DB

---
+++ backend-tram-tri-thuc/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Backend API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
